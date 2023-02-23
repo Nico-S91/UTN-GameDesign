29f08c1effa5e2ec8d749327791792b0b5ef0c39
Nico-S91 <Nico-S91@users.noreply.github.com>
2023-02-22 23:01:48 -0300

    added bomb explosion logic

--- Assets/Scripts/Bomba.cs
+++ Assets/Scripts/Bomba.cs
@@ -4,11 +4,33 @@ using UnityEngine;
 
 public class Bomba : MonoBehaviour
 {
+    public float tiempoDetonacion;
+
+    private bool explotar = false;
+
+    private void Awake()
+    {
+        Invoke("Explotar", tiempoDetonacion);
+    }
+
     void Update()
     {
         if (Input.GetButtonDown("Explotar"))
         {
+            explotar = true;
+        }
+    }
+
+    private void OnTriggerStay(Collider c)
+    {
+        if (explotar)
+        {
+            Destroy(c.gameObject);
             Destroy(gameObject);
         }
+    }
+
+    void Explotar() {
+        explotar = true;
     }
 }

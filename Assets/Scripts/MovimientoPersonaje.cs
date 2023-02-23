2e50529d226712e8b84377755b410c7b1311af3c
Nico-S91 <Nico-S91@users.noreply.github.com>
2023-02-22 21:32:05 -0300

    adding forward and backward movement, plus rotation logic scripts.

---
+++ Assets/Scripts/MovimientoPersonaje.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoPersonaje : MonoBehaviour
+{
+    public float velocidadMovimiento;
+    public float velocidadRotacion;
+
+
+    void Update()
+    {
+        if (Input.GetButton("Arriba"))
+        {
+            gameObject.transform.Translate(velocidadMovimiento, 0, 0);
+        }
+
+        if (Input.GetButton("Abajo"))
+        {
+            gameObject.transform.Translate(-velocidadMovimiento, 0, 0);
+        }
+
+        if (Input.GetButton("Derecha"))
+        {
+            gameObject.transform.Rotate(0, velocidadRotacion, 0);
+        }
+
+        if (Input.GetButton("Izquierda"))
+        {
+            gameObject.transform.Rotate(0, -velocidadRotacion, 0);
+        }
+
+    }
+}

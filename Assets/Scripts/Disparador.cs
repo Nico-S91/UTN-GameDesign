03ca500362985f996f0a864ede8c0bee38eee361
Nico-S91 <Nico-S91@users.noreply.github.com>
2023-02-22 22:54:42 -0300

    Modified player firing so that invokes function and stops shooting when lifting the bey up

--- Assets/Scripts/Disparador.cs
+++ Assets/Scripts/Disparador.cs
@@ -6,13 +6,25 @@ public class Disparador : MonoBehaviour
 {
     public GameObject prefab;
     public string nombreAccion;
+    public float cadenciaDisparo;
 
-    
+
     void Update()
     {
         if (Input.GetButtonDown(nombreAccion))
         {
-            Instantiate(prefab, gameObject.transform.position, gameObject.transform.rotation);
+            InvokeRepeating("Disparar", 0f, cadenciaDisparo);
         }
+
+        if (Input.GetButtonUp(nombreAccion))
+        {
+            CancelInvoke("Disparar");
+        }
+
     }
+
+    void Disparar() {
+        Instantiate(prefab, gameObject.transform.position, gameObject.transform.rotation);
+    }
+
 }

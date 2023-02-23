da4be853347e05c9279936562ae460de32bb4e90
Nico-S91 <Nico-S91@users.noreply.github.com>
2023-02-22 23:23:59 -0300

    Added speed bonus logic

---
+++ Assets/Scripts/VelocidadPowerUp.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VelocidadPowerUp : MonoBehaviour
+{
+    public float bonus;
+
+    private void OnTriggerEnter(Collider c)
+    {
+        MovimientoPersonaje movimientoPersonaje = c.gameObject.GetComponent<MovimientoPersonaje>();
+        movimientoPersonaje.velocidadMovimiento += bonus;
+    }
+
+}

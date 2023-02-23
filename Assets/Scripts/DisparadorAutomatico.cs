5f9ae2186487dad12b551a45390803c89700a56c
Nico-S91 <Nico-S91@users.noreply.github.com>
2023-02-22 22:40:24 -0300

    modified the automatic firing for enemies

--- Assets/Scripts/DisparadorAutomatico.cs
+++ Assets/Scripts/DisparadorAutomatico.cs
@@ -5,10 +5,16 @@ using UnityEngine;
 public class DisparadorAutomatico : MonoBehaviour
 {
     public GameObject prefab;
+    public float cadenciaTiro;
 
 
     private void Awake()
     {
+        InvokeRepeating("Disparar", 0f, cadenciaTiro);
+    }
+
+    void Disparar() {
         Instantiate(prefab, gameObject.transform.position, gameObject.transform.rotation);
     }
+
 }
